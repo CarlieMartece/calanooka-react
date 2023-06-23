423068a726ed316646b8e6617110d1f58bd721fd
CarlieMartece <carliemartece@gmail.com>
2023-06-23 12:45:33 +0100

    add Hew Espireth info and render in ChromaSphere component

--- src/components/Chroma.jsx
+++ src/components/Chroma.jsx
@@ -2,7 +2,6 @@ import data from "../data/chromaSphereSystem.json";
 import SphereCard from "./SphereCard";
 
 export default function Chroma() {
-  console.log(data);
   return (
     <main>
       <div id="Chroma">
--- src/components/ChromaSphere.jsx
+++ src/components/ChromaSphere.jsx
@@ -1,5 +1,30 @@
-export default function ChromaSphere () {
-    return (
-        <h1>Sphere</h1>
-    )
+import { useParams } from "react-router-dom";
+import data from "../data/chromaSphereSystem.json";
+
+export default function ChromaSphere() {
+  const { sphereId } = useParams();
+  const sphere = data.chroma.find((object) => object.id === sphereId);
+  return (
+    <main>
+      <h1>{sphere.name}</h1>
+      <span className="subtitle">({sphere.anagram})</span>
+      <h2>Diameter: </h2>
+      <span className="subtitle">{sphere.diameter}</span>
+      <h2>Colour: </h2>
+      <span className="subtitle">{sphere.colour}</span>
+      <h2>Citizens: </h2>
+      <span className="subtitle">{sphere.citizens}</span>
+      <h2>Traits: </h2>
+      <span className="subtitle">{sphere.traits}</span>
+      <h2>Weapons: </h2>
+      <span className="subtitle">{sphere.weapons}</span>
+      <h2>Further Information: </h2>
+      <ul className="description">
+        {sphere.description.map((paragraph) => {
+          const descKey = paragraph.slice(0, 14);
+          return <li key={descKey}>{paragraph}</li>;
+        })}
+      </ul>
+    </main>
+  );
 }
