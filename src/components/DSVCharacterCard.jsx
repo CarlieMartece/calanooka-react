4ca30747aa15be5806b82e1e4d9d30e9f67daea9
CarlieMartece <carliemartece@gmail.com>
2023-06-25 13:11:31 +0100

    add first two DSV char pics and render on DSV page

--- src/components/DSVCharacterCard.jsx
+++ src/components/DSVCharacterCard.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 
 export default function DSVCharacterCard({ id, name }) {
-    const link = `/dsv/${id}`;
+  const link = `/dsv/${id}`;
+  const imgName = name.toLowerCase().split(" ").join("-");
+  const imgSrc = require(`../images/preview/gc1-${imgName}.jpg`);
 
   return (
     <li>
       <Link to={link}>
+        <img 
+          alt={name}
+          src={imgSrc}
+        />
         <h2>{name}</h2>
       </Link>
     </li>
